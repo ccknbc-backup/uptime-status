c2c3f7535fcf9e568573eb1a62d16cf2a2b8182a
CC康纳百川 <ccknbc@qq.com>
2021-04-28 13:49:54 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 1 小时</p>
+        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
         <p>&copy; 2021 <Link to="https://www.ccknbc.cc/" text="CCKNBC" /></p>
       </div>
     </div>
